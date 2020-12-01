234e756e31a06fbbc1301b5d38772d5a43b15800
Mark Thebault <markthebault@gmail.com>
2020-12-01 14:10:49 +0100

    wip titanic dataset

---
+++ test_titanic.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+aws s3 cp ./data/titanic.csv s3://mth-dlk-metastore/data/titanic.csv
+
+CREATE SCHEMA samples;
+
+CREATE TABLE samples.titanic (
+  passengerid int, -- unique id
+  survived int, -- target label
+  pclass int,
+  name varchar,
+  sex varchar,
+  age int,
+  sibsp int, -- Number of Siblings/Spouses Aboard
+  parch int, -- Number of Parents/Children Aboard
+  ticket varchar,
+  fare double,
+  cabin varchar,
+  embarked varchar
+) 
+WITH (
+  csv_escape = '\',
+  csv_quote = '"',  
+  csv_separator = ',',
+  external_location = 's3a://mth-dlk-metastore/data/titanic.csv/',
+  format = 'CSV'
+);
+
+
+
+#### Azure
+CREATE SCHEMA hive.tpcds WITH (location = 'abfs://raw@fsmthdlk.dfs.core.windows.net/metastore/tpcds');
