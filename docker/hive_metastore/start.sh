d0804eea2185be1e9471dcbe7a0edcbc28443f05
Mark Thebault <markthebault@gmail.com>
2020-12-01 14:04:27 +0100

    possibility to use env vars for conf in hive metastore

---
+++ docker/hive_metastore/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+cat /opt/hive-metastore/conf/metastore-site.xml.tmpl | envsubst > /opt/hive-metastore/conf/metastore-site.xml
+cat /opt/hadoop/etc/hadoop/core-site.xml.tmpl | envsubst > /opt/hadoop/etc/hadoop/core-site.xml
+
+/opt/hive-metastore/bin/start-metastore -p $METASTORE_PORT
