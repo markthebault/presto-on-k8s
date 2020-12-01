0379d944c7b9c30af5f811fd9292df08e7767204
Mark Thebault <markthebault@gmail.com>
2020-12-01 14:09:36 +0100

    possibility to configure Azure creds

--- docker/presto/autoconfig_and_launch.sh
+++ docker/presto/autoconfig_and_launch.sh
@@ -6,7 +6,13 @@ cp /opt/presto-server/etc/node.properties.template /opt/presto-server/etc/node.p
 echo "node.id=$HOSTNAME" >> /opt/presto-server/etc/node.properties
 
 cp /opt/presto-server/etc/catalog/hive.properties.template /opt/presto-server/etc/catalog/hive.properties
+
+#AWS
 echo "hive.s3.aws-access-key=$AWS_ACCESS_KEY_ID" >> /opt/presto-server/etc/catalog/hive.properties
 echo "hive.s3.aws-secret-key=$AWS_SECRET_ACCESS_KEY" >> /opt/presto-server/etc/catalog/hive.properties
+
+#Azure
+echo "hive.azure.abfs-storage-account=$ARM_SA_NAME" >> /opt/presto-server/etc/catalog/hive.properties
+echo "hive.azure.abfs-access-key=$ARM_SA_KEY" >> /opt/presto-server/etc/catalog/hive.properties
 
 /opt/presto-server/bin/launcher run
